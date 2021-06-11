78b55e9634e67f9ca642dd7d67db93d140c9d64a
sarah <skg@neofilm.us>
2021-06-10 21:49:50 -0400

    Begin functionality for creating new notes.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-$(document).ready(function () {
-	
+$(document).ready(function() {
+    var id = 1;
+	$("#btnNew").click(function(){
+        var newNote = "$('<div class='sticky greeen' id='sticky" + id +"'><textarea>Add Your Note</textarea></div>)draggable()";
+        id++;
+    });
 });
