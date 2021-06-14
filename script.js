ae3d86034ee5836ec40b4e1e41edc13e3e63f7df
sarah <skg@neofilm.us>
2021-06-14 17:39:22 -0400

    Style the new notes for basic function.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var id = 1;
 	$("#btnNew").click(function(){
-        var newNote = "$('<div class='sticky greeen' id='sticky" + id +"'><textarea>Add Your Note</textarea></div>)draggable()";
+        var newNote = $("<div class='sticky green' id='sticky" + id +"'><textarea>Add Your Note</textarea></div>").draggable();
         id++;
         $("#container").append(newNote);
     });
