9fa92de91875d0bf0252d3c996c377945db65c7b
sarah <skg@neofilm.us>
2021-06-15 15:56:20 -0400

    Implement color change functionality.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     var id = 1;
-	$("#btnNew").click(function(){
+    var currentElement = "";
+	$("#btnNew").click(function() {
         var newNote = $("<div class='sticky green' id='sticky" + id +"'><textarea>Add Your Note</textarea></div>").draggable();
+        currentElement = "sticky" + id;
         id++;
         $("#container").append(newNote);
+    });
+    $(".box").click(function() {
+        if (currentElement != "") {
+            var color = $(this).attr("class").split(" ")[0];
+            $("#" + currentElement).addClass(color);
+        }
     });
 });
